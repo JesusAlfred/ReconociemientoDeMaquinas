7896f5645e1975992b3348017e307932cab73204
Paulo <paulozamora2000@gmail.com>
2022-10-29 21:56:03 -0500

    script format, aditional search terms

--- dataPrep/imageDownload.py
+++ dataPrep/imageDownload.py
@@ -4,54 +4,58 @@ import os
 import shutil
 import pickle
 
+
 def my_progressbar(url, progress):
     print(url[:10] + ' ' + str(progress) + '%')
 
+
 dir = './../img/'
 try:
-  os.mkdir(dir)
+    os.mkdir(dir)
 except Exception as e:
-  print(e)
-  shutil.rmtree(dir)
-  os.mkdir(dir)
+    print(f"Excepcion creating dir {dir}: {e}")
 
 #gis = GoogleImagesSearch('AIzaSyA0pSMDVGYmag6rpSNNRE1f4jjDjiF4qMU', '52431cda185b04ae5', progressbar_fn=my_progressbar)
-gis = GoogleImagesSearch('AIzaSyA2uvjsmG_XSG-5NmneCvZ7IRnh5Ke4tXA', '52ed765d7c57849b7', progressbar_fn=my_progressbar)
+gis = GoogleImagesSearch('AIzaSyA2uvjsmG_XSG-5NmneCvZ7IRnh5Ke4tXA',
+                         '52ed765d7c57849b7', progressbar_fn=my_progressbar)
 
 listaDeBusquedas = [
-  'weight training bench',
-  'excercise cable machine'
+    'weight training bench',
+    'excercise cable machine',
+    'gym bench'
+    'gym cable machine',
+    'bench press bench'
 ]
 
 
 for search in listaDeBusquedas:
-  gis = GoogleImagesSearch('AIzaSyA2uvjsmG_XSG-5NmneCvZ7IRnh5Ke4tXA', '52ed765d7c57849b7', progressbar_fn=my_progressbar)
-  _search_params = {
-      'q': search,
-      'num': 100,
-      'fileType': 'jpg|gif|png',
-  }
+    gis = GoogleImagesSearch('AIzaSyA2uvjsmG_XSG-5NmneCvZ7IRnh5Ke4tXA',
+                             '52ed765d7c57849b7', progressbar_fn=my_progressbar)
+    _search_params = {
+        'q': search,
+        'num': 100,
+        'fileType': 'jpg|gif|png',
+    }
 
-  # this will search, download and resize:
-  
-  gis.search(search_params=_search_params)
+    # this will search, download and resize:
 
-  data = gis.results()
-  file = open('./datos'+search+'.pc', 'wb')
-  pickle.dump(data, file)
-  file.close()
+    gis.search(search_params=_search_params)
 
+    data = gis.results()
+    file = open('./datos'+search+'.pc', 'wb')
+    pickle.dump(data, file)
+    file.close()
 
-  file = open('./datos'+search+'.pc', 'rb')
-  data = pickle.load(file)
-  file.close()
+    file = open('./datos'+search+'.pc', 'rb')
+    data = pickle.load(file)
+    file.close()
 
-  for image in data:
-    print(image)
-    image.url  # image direct url
-    image.referrer_url  # image referrer url (source) 
-    
-    image.download(dir+search+'/')  # download image
-    image.resize(500, 500)  # resize downloaded image
+    for image in data:
+        print(image)
+        image.url  # image direct url
+        image.referrer_url  # image referrer url (source)
 
-    image.path  # downloaded local file path
+        image.download(dir+search+'/')  # download image
+        image.resize(500, 500)  # resize downloaded image
+
+        image.path  # downloaded local file path
